06267768d738a389c186198f897c4841685dae42
Dasha <darya.pankova13@gmail.com>
2017-07-02 13:12:34 +0300

    CRUD controller implemented

--- src/main/java/com/pankova/chatbot/controller/TodoController.java
+++ src/main/java/com/pankova/chatbot/controller/TodoController.java
@@ -1,42 +1,51 @@
 package com.pankova.chatbot.controller;
 
-import java.util.List;
-
-import javax.websocket.server.PathParam;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pankova.chatbot.model.Task;
+import com.pankova.chatbot.repository.TaskRepository;
 
 @RestController
 @RequestMapping(path = "/todo")
 public class TodoController {
-	
+
+	@Autowired
+	private TaskRepository taskRepository;
+
 	@RequestMapping(path = "", method = RequestMethod.POST)
-	public Task create(@RequestBody Task task){
-		return task;
+	public Task create(@RequestBody Task task) {
+		return taskRepository.save(task);
 	}
-	
+
 	@RequestMapping(path = "", method = RequestMethod.GET)
-	public List<Task> readAll(){
-		return null;
+	public Iterable<Task> readAll() {
+		return taskRepository.findAll();
 	}
 
 	@RequestMapping(path = "/{id}", method = RequestMethod.GET)
-	public Task read(@PathParam(value = "id") Long id){
-		return null;
+	public Task read(@PathVariable long id) {
+		return taskRepository.findOne(id);
 	}
-	
-	@RequestMapping(path = "/{id}", method = {RequestMethod.PUT, RequestMethod.PATCH})
-	public Task update(@RequestBody Task task, @PathParam(value = "id") Long id){
-		return task;
+
+	@RequestMapping(path = "/{id}", method = { RequestMethod.PUT,
+			RequestMethod.PATCH })
+	public Task update(@RequestBody Task task, @PathVariable long id) {
+		Task existingTask = taskRepository.findOne(id);
+		Task result = null;
+		if (existingTask != null) {
+			task.setId(existingTask.getId());
+			result = taskRepository.save(task);
+		}
+		return result;
 	}
-	
+
 	@RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
-	public Void delete(@PathParam(value = "id") Long id){
-		return null;
+	public void delete(@PathVariable long id) {
+		taskRepository.delete(id);
 	}
 }
